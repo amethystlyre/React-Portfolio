291fc0027640b6f0109584e8598b1c73b72e4bad
amethystlyre <91407728+amethystlyre@users.noreply.github.com>
2024-01-22 00:54:42 +1100

    fix broken link after deployed

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -2,7 +2,7 @@ export default function About() {
   return (
     <div>
       <h1>Amethyst</h1>
-      <img src="/src/assets/avatar.gif"/>
+      <img src="./src/assets/avatar.gif"/>
       <h3>About Me</h3>
       <div><p>
        Full stack web development student learning React and MERN technologies.
