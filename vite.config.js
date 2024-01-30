9b893c43de38b2c02717bc4cafafe3476ea179c8
amethystlyre <91407728+amethystlyre@users.noreply.github.com>
2024-01-31 01:14:30 +1100

    heroku

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  mode:'development',
   plugins: [react()],
   server: {
-    port: process.env.PORT || 3000,
+    host: true,
+    port: 3000,
     open: true
   }
 })
